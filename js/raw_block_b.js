2cffcc49aecff3ae49b0cd41a1cf4906869eeb7c
zhaoyilun <zhaoyilun@outlook.com>
2019-09-24 11:50:27 +0800

    first commit

---
+++ js/raw_block_b.js
@@ -0,0 +1,10 @@
+Blockly.Blocks['raw'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("raw(5)");
+    this.setOutput(true, null);
+    this.setColour(300);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
